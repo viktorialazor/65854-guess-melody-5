82927b79aef961ea9027968cae2907a2922d93c1
Виктория Лазор <viktoria.lazor@gmail.com>
2020-10-22 15:41:33 +0300

    Training project: the other side of reality

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -7,21 +7,34 @@ import GenreQuestionScreen from "../genre-question-screen/genre-question-screen"
 import AuthScreen from "../auth-screen/auth-screen";
 import GameOverScreen from "../game-over-screen/game-over-screen";
 import WinScreen from "../win-screen/win-screen";
+import GameScreen from "../game-screen/game-screen";
 
-const App = ({errorsCount}) => {
+const App = ({errorsCount, questions}) => {
+  const [firstQuestion, secondQuestion] = questions;
 
   return (
-
     <BrowserRouter>
       <Switch>
-        <Route exact path="/">
-          <WelcomeScreen errorsCount={errorsCount} />
-        </Route>
+        <Route exact
+          path="/"
+          render={({history}) => (
+            <WelcomeScreen
+              onPlayButtonClick={() => history.push(`/game`)}
+              errorsCount={errorsCount}
+            />
+          )}
+        />
         <Route exact path="/dev-artist">
-          <ArtistQuestionScreen />
+          <ArtistQuestionScreen
+            question={secondQuestion}
+            onAnswer={() => {}}
+          />
         </Route>
         <Route exact path="/dev-genre">
-          <GenreQuestionScreen />
+          <GenreQuestionScreen
+            question={firstQuestion}
+            onAnswer={() => {}}
+          />
         </Route>
         <Route exact path="/login">
           <AuthScreen />
@@ -32,6 +45,12 @@ const App = ({errorsCount}) => {
         <Route exact path="/lose">
           <GameOverScreen />
         </Route>
+        <Route exact path="/game">
+          <GameScreen
+            errorsCount={errorsCount}
+            questions={questions}
+          />
+        </Route>
       </Switch>
     </BrowserRouter>
   );
@@ -39,6 +58,7 @@ const App = ({errorsCount}) => {
 
 App.propTypes = {
   errorsCount: PropTypes.number.isRequired,
+  questions: PropTypes.array.isRequired,
 };
 
 export default App;
